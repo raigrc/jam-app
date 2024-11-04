2330e710a1662c8645828c6e166df9223c36da4e
raigrc <ravenjstn.grc@gmail.com>
2024-11-04 14:28:13 +0800

    remove globar prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-const prismaInstance = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismaInstance;
-
-export const prisma = prismaInstance;
+export const prisma = new PrismaClient();
